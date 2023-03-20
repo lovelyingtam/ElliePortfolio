7b57022a43385ca2778eabec868ca44c9d4bcb52
Ellie Tam <>
2023-03-20 17:59:39 -0400

    Added JankStats library to helps tracking and analyzing performance problems in your applications. Jank refers to application frames that take too long to render, and the JankStats library provides reports on the jank statistics of your app.

--- app/src/main/java/com/ellie/jetportfolio/MainActivity.kt
+++ app/src/main/java/com/ellie/jetportfolio/MainActivity.kt
@@ -3,12 +3,21 @@ package com.ellie.jetportfolio
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.metrics.performance.JankStats
 import com.ellie.jetportfolio.ui.JetPortfolioApp
 import com.ellie.jetportfolio.ui.theme.JetPortfolioTheme
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    /**
+     * Lazily inject [JankStats], which is used to track jank throughout the app.
+     */
+    @Inject
+    lateinit var lazyStats: dagger.Lazy<JankStats>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -16,5 +25,15 @@ class MainActivity : ComponentActivity() {
                 JetPortfolioApp()
             }
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        lazyStats.get().isTrackingEnabled = true
+    }
+
+    override fun onPause() {
+        super.onPause()
+        lazyStats.get().isTrackingEnabled = false
     }
 }
